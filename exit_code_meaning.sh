1cb2286766984da55306341d0d7c33f96b254028
Mayur Nagekar <mayur.nagekar@gmail.com>
2014-04-14 14:06:57 +1000

    Added signal specific exit codes.

--- exit_code_meaning.sh
+++ exit_code_meaning.sh
@@ -6,31 +6,87 @@ function tell_me_meaning_of_exit_code {
    echo Parameter passed :  ${exit_code}
 
     echo "Exit code ${exit_code}"
+
     case "${exit_code}" in 
      
        1)
-       echo "Catchall for general errors." ;; 
+       echo "Catchall for general errors."
+       ;; 
 
        2)
-       echo "Misuse of shell builtins." ;;
+       echo "Misuse of shell builtins." 
+       ;;
 
        126)
-       echo "Command invoked cannot be executed.Possibly a permission issue!";;
+       echo "Command invoked cannot be executed.Possibly a permission issue!"
+       ;;
 
        127)
-       echo "Command invoked not found.";;
+       echo "Command invoked not found."
+       ;;
 
        128)
-       echo "Invalid argument to exit.Exit takes only integer args in the range 0-255.";;
+       echo "Invalid argument to exit.Exit takes only integer args in the range 0-255."
+       ;;
 
-       130)
-       echo "Script terminated by Control-C.";;
+       #130)
+       #echo "Script terminated by Control-C.";;
 
        255)
-       echo "Exit status out of range.";;
+       echo "Exit status out of range."
+       ;;
 
        *)
-       echo "Unknown exit code ${exit_code}. I will investigate" ;;    
+       if [ ${exit_code} -gt 128 ] && [ ${exit_code} -le 192 ] ; then
+           echo "Exit code  is between 128 and 192."
+           signal_code=$( expr $exit_code - 128 )
+           echo SIGNAL CODE = ${signal_code}
+       
+             case ${signal_code} in 
+
+#1) SIGHUP	 2) SIGINT	 3) SIGQUIT	 4) SIGILL	 5) SIGTRAP
+#6) SIGABRT	 7) SIGBUS	 8) SIGFPE	 9) SIGKILL	10) SIGUSR1
+#11) SIGSEGV	12) SIGUSR2	13) SIGPIPE	14) SIGALRM	15) SIGTERM
+#16) SIGSTKFLT	17) SIGCHLD	18) SIGCONT	19) SIGSTOP	20) SIGTSTP
+#21) SIGTTIN	22) SIGTTOU	23) SIGURG	24) SIGXCPU	25) SIGXFSZ
+#26) SIGVTALRM	27) SIGPROF	28) SIGWINCH	29) SIGIO	30) SIGPWR
+#31) SIGSYS	34) SIGRTMIN	
+
+
+                  
+                  1)
+                  echo "SIGHUP"
+                  ;;
+ 
+                  2)
+                  echo "SIGINT"
+                  ;;
+
+                  3)
+                  echo "SIGQUIT"
+                  ;;
+
+                  4)
+                  echo "SIGILL"
+                  ;;
+
+                  5)
+                  echo "SIGTRAP"
+                  ;;
+
+                  6)
+                  echo "SIGABRT"
+                  ;;
+
+                  7)
+                  echo "SIGBUS"
+                  ;;
+          esac
+
+       else
+           echo "Unknown exit code ${exit_code}. I will investigate"    
+       fi
+       ;;
 
     esac
 
