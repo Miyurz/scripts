1be550cf47cfd4db970253bd3c051387a48fc109
Mayur Nagekar <mayur.nagekar@gmail.com>
2014-06-03 14:29:14 +0530

    Developmnet binaries

--- vagrant_tool/development_tools_and_libraries.sh
+++ vagrant_tool/development_tools_and_libraries.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+function install_package {
+
+  apt-get install ${package} -y &> log
+
+  if [ $? == 0 ];
+  then
+    echo ${package} installed via apt-get successfully.
+  else
+    echo ${package} did not install right.
+    exit 1;
+  fi
+
+}
+
+apt-get update
+
+echo "Install necessary editors for developers"
+install_package vim
+
 echo "Download source of development libaries and install them"
 
-echo ncurses,expat-devel,docbook2texi,cpan,pyreadline
+declare -a packages=('make' \
+                     'gcc' \ 
+                     'libcurl4-gnutls-dev' \
+                     'libcurl4-openssl-dev' \
+                     'gcc-multilib' \
+                     'lib64expat1-dev');
+
+
+for package in "${packages[@]}" 
+do
+   echo Software package to be installed : ${package}
+   install_package ${package}
+done
+
+
--- vagrant_tool/install_git.sh
+++ vagrant_tool/install_git.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-sudo apt-get remove git -y
-sudo apt-get install vim -y
-sudo apt-get install make -y
-sudo apt-get install gcc -y
-sudo apt-get install libcurl4-gnutls-dev -y
-sudo apt-get install libcurl4-openssl-dev -y
-sudo apt-get install lib64expat1-dev -y
-sudo apt-get install tcl
-
+apt-get remove git
 
 echo "Install latest git for source available on code.google.com"
 
