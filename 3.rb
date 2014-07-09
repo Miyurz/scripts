04fdd4cd45fdc167fa61da2fe0d46e2ecdd002b4
Mayur Nagekar <mayur.nagekar@gmail.com>
2014-07-09 23:09:27 +0530

    Some more encryption methods added to 3.rb

--- 3.rb
+++ 3.rb
@@ -2,7 +2,7 @@
 
 require 'digest'
 
-def encryption(encryptionType="MD5",stringToBeEncrypted="message")
+def encryption(encryptionType="MD5",stringToBeEncrypted="message",salt="test")
 
 puts "Encryption Type passed: #{encryptionType}"
 puts "Encrypting string: #{stringToBeEncrypted}"
@@ -11,6 +11,16 @@ puts "Encrypting string: #{stringToBeEncrypted}"
   when "SHA256"
     puts "Encrypting #{stringToBeEncrypted} with SHA256 algorithm"
     puts Digest::SHA256.digest '#{stringToBeEncrypted}'
+  when "SHA384"
+    puts "Encrypting #{stringToBeEncrypted} with SHA384 algorithm"
+    puts Digest::SHA384.digest '#{stringToBeEncrypted}'
+  when "SHA512"
+    puts "Encrypting #{stringToBeEncrypted} with SHA512 algorithm"
+    puts Digest::SHA512.digest '#{stringToBeEncrypted}'
+  when "HMAC"
+    puts "Encrypting #{stringToBeEncrypted} with HMAC algorithm and salt, #{salt}"
+    puts Digest::HMAC.hexdigest("#{stringToBeEncrypted}", "#{salt}", Digest::SHA1)
+    #puts Digest::HMAC.hexdigest '#{stringToBeEncrypted}'
   when "MD5"
     puts "Encrypting #{stringToBeEncrypted} with MD5 algorithm"
     puts Digest::MD5.hexdigest '#{stringToBeEncrypted}'
@@ -20,8 +30,10 @@ puts "Encrypting string: #{stringToBeEncrypted}"
 
 end
 
-arg1 = ARGV[0]
-arg2 = ARGV[1]
+encryption = ARGV[0]
+string = ARGV[1]
+#Third argument should be the salt that is needed by some like the HMAC keyed-hashing algorithm
+saltPassed = ARGV[2]
 
-encryption "#{arg1}","#{arg2}"
+encryption "#{encryption}","#{string}","#{saltPassed}"
 #encryption "SHA","mayur"
